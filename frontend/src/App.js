f7582a14791ff66f28e6d87d45dcf696c13da6f7
Neylane <neylanepl@gmail.com>
2022-12-03 22:46:29 -0300

    feat: página inicio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 import CadastrarBovino from './pages/bovino/cadastrarBovino';
+import Inicio from './pages/inicio/inicio';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes> 
+          <Route path='/' element={<Inicio/>} />
           <Route path='/cadastrarbovino' element={<CadastrarBovino/>} />
       </Routes>
     </BrowserRouter>
