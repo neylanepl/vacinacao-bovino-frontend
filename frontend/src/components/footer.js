51c75986bce3e85b53c9f26d3765d0dd6d97258f
Neylane <neylanepl@gmail.com>
2022-12-03 21:43:54 -0300

    feat: componentes nav e footer

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return(
+        <div className="footer fixed-bottom" style={{background: "#83A93A", height: "10%"}}> </div>
+    );
+}
+
+export default Footer;
