d925366bc625eb9189372132286ed2d86077b09d
Neylane <neylanepl@gmail.com>
2022-12-04 00:58:37 -0300

    feat:  página da carteira de vacinação

---
+++ frontend/src/pages/aplicacao/carteiraVacinacao.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import Nav from '../../components/nav';
+import Footer from '../../components/footer';
+
+const CarteiraVacinacao = () => {
+    const navigate = useNavigate();
+
+    return(
+        <div>
+            <Nav/>
+            <p className="fs-1 text-center" style={{background: "#E0E7CA", padding: "20px"}}>Carteira de Vacinação</p>
+        
+            <div className="guardaBotao">
+                    <button type="submit" className="botao" style={{backgroundColor:"#83A93A",borderColor: "#6D3B00"}} variant="warning"  onClick={e => navigate('/cadastraraplicacao')}>
+                        Cadastrar aplicação de vacina
+                    </button>
+            </div>
+
+            <div style={{margin: "5%"}}>
+
+                <div style={{marginLeft: "20%", marginRight: "20%", background: "#E0E7CA"}} >
+                    <table className="table table-borderless" >
+                        <tbody className="text-center">
+                            <tr>
+                                <td>
+                                    ID: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.id}"></span>
+                                </td>  
+                                <td>
+                                    Peso: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.peso}"></span>
+                                    kg
+                                </td> 
+                                <td>
+                                    Data de nascimento: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.aniversario}"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nome: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.nome}"></span>
+                                </td> 
+                                <td>
+                                    Sexo: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.sexo}"></span>
+                                </td>   
+                                <td>
+                                    Cor: 
+                                    <span text="${carteira.isEmpty()} ? 'none' : ${carteira.get().bovino.cor}"></span>
+                                </td>           
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div style={{marginLeft: "20%", marginRight: "20%", marginBottom: "10%"}}>
+                    <table className="table table-bordered table-bordered" >
+                        <div className="text-center" style={{background: "#E0E7CA", padding: "1%"}}><strong>Carteira de Vacinação</strong></div>
+                        <tbody>
+                            <tr>
+                                <td>Coronavac
+                                    Dose 1
+                                    20/02/2022</td>
+                            </tr>
+                            <tr>
+                                <td>Pfizer
+                                    Dose 3
+                                    20/02/2022</td>
+                            </tr>
+                            <tr>
+                                <td>Jenssen
+                                    Dose 1
+                                    20/02/2022</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export default CarteiraVacinacao;
+
