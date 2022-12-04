9f3d069f79630364734f29002a5d734409f7ee3f
Neylane <neylanepl@gmail.com>
2022-12-03 23:59:05 -0300

    feat: rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Inicio from './pages/inicio/inicio';
 import CadastrarBovino from './pages/bovino/cadastrarBovino';
+import EditarBovino from './pages/bovino/editarBovino';
 import CadastrarAplicacao from './pages/aplicacao/cadastrarAplicacao';
+import CarteiraVacinacao from './pages/aplicacao/carteiraVacinacao';
+import ListaAnimaisCarteiraVacinacao from './pages/aplicacao/listaAnimaisCarteiraVacinacao';
+import CadastrarVacina from './pages/vacina/cadastrarVacina';
+import EditarVacina from './pages/vacina/editarVacina';
+import ListarVacinas from './pages/vacina/listarVacinas';
 
 
 function App() {
@@ -12,7 +18,13 @@ function App() {
       <Routes> 
           <Route path='/' element={<Inicio/>} />
           <Route path='/cadastrarbovino' element={<CadastrarBovino/>} />
+          <Route path='/editarbovino' element={<EditarBovino/>} />
           <Route path='/cadastraraplicacao' element={<CadastrarAplicacao/>} />
+          <Route path='/carteiravacinacao' element={<CarteiraVacinacao/>} />
+          <Route path='/listaanimaiscarteiravacinacao' element={<ListaAnimaisCarteiraVacinacao/>} />
+          <Route path='/cadastrarvacina' element={<CadastrarVacina/>} />
+          <Route path='/editarvacina' element={<EditarVacina/>} />
+          <Route path='/listarvacinas' element={<ListarVacinas/>} />
       </Routes>
     </BrowserRouter>
 
