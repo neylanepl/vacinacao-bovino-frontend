13c36984db758d4e45c96cd9be47873f340233cb
Neylane <neylanepl@gmail.com>
2022-12-04 02:05:57 -0300

    fix: rota de vacina e bovino

--- frontend/src/pages/inicio/inicio.js
+++ frontend/src/pages/inicio/inicio.js
@@ -17,9 +17,9 @@ const Inicio = () => {
             <h1 className="fs-1 text-center" style={{background: "#E0E7CA", padding: "20px", marginBottom: "5%"}}> Página Inicial</h1>
             
                 <div className="text-center" style={{}}>        
-                    <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/')}>Vacinas</button></div>
+                    <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/listarvacinas')}>Vacinas</button></div>
                     <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/')}>Fabricantes de vacinas</button></div>
-                    <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/cadastrarbovino')}>Bovinos</button></div>
+                    <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/listaanimaiscarteiravacinacao')}>Bovinos</button></div>
                     <div><button variant="warning"  type="submit" value="submit" className="btn" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", width: "40%", margin: "20px"}} onClick={e => navigate('/')}>Usuários</button> </div>
                
             </div>
