53fd9b9939ce24bab76c1be8f61c2109867bf6ed
Neylane <neylanepl@gmail.com>
2022-12-04 00:59:00 -0300

    feat: página de editar bovino

---
+++ frontend/src/pages/bovino/editarBovino.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import { Form } from 'react-bootstrap';
+import Nav from '../../components/nav';
+import Footer from '../../components/footer';
+
+const EditarBovino = () => {
+    const [NomeForm, setNomeForm] = useState('');
+    const [pesoForm, setPesoForm] = useState(0);
+    const [dataForm, setDataForm] = useState('');
+    const [sexoForm, setSexoForm] = useState('');
+    const [corForm, setCorForm] = useState('');
+
+    const handleSubmitForm = async e => {}
+
+    return(
+        <div>
+            <Nav/>
+            <h1 className="fs-1 text-center" style={{background: "#E0E7CA", padding: "20px"}}> Editar bovino </h1>
+            <div className="formularioCadastroBovino" style={{marginBottom: "10%"}}>
+                <form className="formulario" onSubmit={e => {handleSubmitForm (e)}}>
+                    <div className="sub-div">
+                        <div className="id_"><p>Nome</p></div> 
+                        <input type="text" className="nomeBovino" onChange={e => setNomeForm(e.target.value)}/>
+
+                        <div className="id_"><p>Peso</p></div> 
+                        <input type="number" className="pesoBovino" onChange={e => setPesoForm(e.target.value)}/>
+
+                        <div className="id_"><p>Idade</p></div> 
+                        <input type="date" className="idadeBovino" onChange={e => setDataForm(e.target.value)}/>
+                            
+                        <div className="id_"><p>Sexo</p></div> 
+                        <div className="id_">
+                            <div className="check-option" style={{marginBottom: "15px", display: "flex", justifyContent: "space-evenly"}}>
+                                <Form.Check type="radio"  id="Femea" label="Femea" onClick={(e) => setSexoForm(e.target.id)}/>
+                                <Form.Check type="radio" id="Macho" label="Macho" onClick={(e) => setSexoForm(e.target.id)}/>
+                            </div>
+                        </div>
+
+                        <div className="id_"><p>Cor</p></div> 
+                        <div className="id_">
+                            <div className="check-option" style={{marginBottom: "15px", display: "flex", justifyContent: "space-evenly"}}>
+                                <Form.Check type="radio" id="Preto" label="Preto" onClick={(e) => setCorForm(e.target.id)}/>
+                                <Form.Check type="radio" id="Branco" label="Branco" onClick={(e) => setCorForm(e.target.id)}/>
+                                <Form.Check type="radio" id="Laranja" label="Laranja" onClick={(e) => setCorForm(e.target.id)}/>
+                                <Form.Check type="radio" id="Amarelo" label="Amarelo" onClick={(e) => setCorForm(e.target.id)}/>
+                                <Form.Check type="radio" id="Vermelho" label="Vermelho" onClick={(e) => setCorForm(e.target.id)}/>  
+                            </div>
+                        </div>
+                        <button variant="warning"  type="submit" value="submit" className="btn btn-success" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", margin: "40px"}}>Salvar</button>
+                    </div>            
+                </form>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export default EditarBovino;
