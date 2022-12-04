3a39e0ab9a5a910a532766200655b6333caef03a
silviafds <silviafernandes974@gmail.com>
2022-12-04 10:34:41 -0300

    feat: ajuste na tela de login

--- frontend/src/pages/pessoa/login.js
+++ frontend/src/pages/pessoa/login.js
@@ -15,15 +15,16 @@ const Login = () => {
     return (
         <div>
             <Nav/>
+            
             <h1 className="fs-1 text-center" style={{background: "#E0E7CA", padding: "20px"}}> Login </h1>
             <div className="formularioLogin" style={{marginBottom: "10%"}}>
-            <form className="formulario" onSubmit={e => {handleSubmitForm (e)}}>
+                <form className="formulario" onSubmit={e => {handleSubmitForm (e)}}>
                     <div className="sub-div">
-                        <div className="id_"><p>Nome</p></div> 
+                        <div className="id_"><p>Email</p></div> 
                         <input type="text" className="nomePessoa" onChange={e => setNomeForm(e.target.value)}/>
 
                         <div className="id_"><p>Senha</p></div> 
-                        <input type="number" className="senhaPessoa" onChange={e => setSenhaForm(e.target.value)}/>
+                        <input type="text" className="senhaPessoa" onChange={e => setSenhaForm(e.target.value)}/>
 
                         <button variant="warning"  type="submit" value="submit" className="btn btn-success" style={{backgroundColor:"#83A93A", borderColor: "#6D3B00", margin: "40px"}}>Entrar</button>
                     </div>            
